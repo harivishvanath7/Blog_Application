8cb5ccf4cba773c5c76facf1b56197e176d19094
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-03 19:39:44 +0530

    feat(models): Added DB models for Users and Posts

---
+++ backend/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,         
+        ref: "User"
+    }  
+}, { timestamps: true });
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
+
+
+// author field stores the User document's ObjectId (reference to Users collection)
