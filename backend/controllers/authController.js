7092107a8f397392c97f4e350d58763610ab867c
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-04 13:13:17 +0530

    feat(controllers): Added Controllers for Auth and Posts

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,69 @@
-const registerUser = (req, res) => {
-    res.json({ message: "User Registered."});
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.json({ message : "User Already Exists" });
+        }
+
+        // Password Hashing
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        res.json({
+            message: "User registered successfully",
+            user
+        });
+    } catch (error) {
+        res.status(500).json( {message: "Server error"});
+    }    
 };
 
-const loginUser = (req, res) => {
-    res.json({ message: "User Logged in."});
+
+
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        return res.json({ message: "User Not Found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        return res.json({ message: "Invalid Password"});
+    }
+
+    // JWT Token creation
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h"}
+    );
+
+    // Sending the token
+    res.json({
+        message: "Login succesful",
+        token
+    });
 };
+
+
 
 module.exports = { registerUser, loginUser };
 
--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,65 @@
-const getPosts = (req, res) => {
-    res.json({ message: "All blog posts"});
+const Post = require("../models/Post");
+
+
+const createPost = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const post = await Post.create({
+            title,
+            content,
+            author: req.user.id
+        });
+
+        res.json(post);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error. "});
+    }
 };
 
-const createPost = (req, res) => {
-    res.json({ message: "Post created"});
+
+const getPosts = async (req, res) => {
+        const posts = await Post.find().populate("author", "username");
+
+        res.json(posts);
 };
 
-module.exports = { getPosts, createPost };
+const getPost = async (req, res) => {
+    const post = await Post.findById(req.params.id).populate("author", "username");
 
+    if(!post) {
+        return res.json({ message: "Post not found" });
+    }
+
+    res.json(post);
+};
+
+const updatePost = async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if(!post) {
+        return res.json({ message: "Post not found" });
+    }
+
+    post.title = req.body.title || post.title;
+    post.content = req.body.content || post.content;
+
+    await post.save();
+
+    res.json(post);
+};
+
+const deletePost = async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if(!post) {
+        return res.json({ message: "Post not found" });
+    }
+
+    await post.deleteOne();
+
+    res.json({ message: "Post Deleted" });
+};
+
+module.exports = { createPost, getPosts, getPost, updatePost, deletePost };
