46cf57d63019a7d41012ebd2aec139cbdb4a18a4
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-04 21:41:01 +0530

    feat(App page): Created basic App page.

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl">Blog App 🚀</h1>
+      <p className="mt-4">Your design system is working!</p>
+    </div>
+  )
+};
+
+export default App;
