dc40e61211627c1dba5b24273b755c7c616f8257
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-04 21:14:02 +0530

    chore: Added DB Connection Import in server.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 // midlleware
 app.use(cors());
 app.use(express.json());
+
+// DB Connection
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("API is running..");
