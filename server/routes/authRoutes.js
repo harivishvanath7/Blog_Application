c4799a04ca18ddb1c2496eb9c260e86ee530b158
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-06 14:13:56 +0530

    routes: Created Auth Routes.

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { loginAdmin } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/login", loginAdmin);
+
+export default router;
+
+
+
+
