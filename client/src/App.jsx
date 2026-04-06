3b3bfbd23d748b47a991ea48e0839fac9ae041e7
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-06 14:42:13 +0530

    app: Added the Updated Routes for Pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import BlogDetail from "./pages/BlogDetail.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
+import CreateBlog from "./pages/CreateBlog.jsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:slug" element={<BlogDetail />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/create" element={<CreateBlog />} />
         </Routes>
       </div>
     </Router>
