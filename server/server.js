643de6eb182941d6890e52299ad0f4a4d230e617
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-06 11:44:00 +0530

    chore: Imported routes in server.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import blogRoutes from "./routes/blogRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,12 +16,15 @@ app.use(express.json());
 // DB Connection
 connectDB();
 
+// Routes
+app.use("/api/blogs", blogRoutes);
+
 app.get("/", (req, res) => {
-    res.send("API is running..");
+  res.send("API is running..");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
