81ce26557ea11aebc92eaa6ca40f3dded77b8535
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-06 12:36:37 +0530

    pages: Created pages for Home and BlogDetail display.

---
+++ client/src/pages/BlogDetail.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../api/api.js";
+
+function BlogDetail() {
+  const { slug } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    API.get(`/blogs/${slug}`)
+      .then((res) => setBlog(res.data))
+      .catch((err) => console.log(err));
+  }, [slug]);
+
+  if (!blog) return <p className="p-6">Loading...</p>;
+
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+
+      <h1 className="text-4xl mb-4 text-primary-dark">
+        {blog.title}
+      </h1>
+
+      <div className="flex gap-3 mb-4 flex-wrap">
+        {blog.tags?.map((tag, i) => (
+          <span
+            key={i}
+            className="bg-secondary text-white px-3 py-1 rounded-full text-sm"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <p className="leading-7 text-lg whitespace-pre-line">
+        {blog.content}
+      </p>
+
+      <p className="mt-6 text-sm text-gray-500">
+        👁 {blog.views} views
+      </p>
+
+    </div>
+  );
+}
+
+export default BlogDetail;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import API from "../api/api.js";
+import BlogCard from "../components/BlogCard.jsx";
+
+
+function Home() {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        API.get("/blogs")
+            .then((res) => setBlogs(res.data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            {/* Hero */}
+            <h1 className="text-4xl mb-8 text-primary-dark">
+                My Blog 
+            </h1>
+
+            {/* Blog Grid */}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {blogs.map((blog) => (
+                    <BlogCard key={blog._id} blog={blog} />
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
