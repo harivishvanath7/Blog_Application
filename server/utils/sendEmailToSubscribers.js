12b53304a36f7179ab0ef49abbfb0da0d1cd5379
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-07 09:56:29 +0530

    update: Added unsubscribe token and mail sending functionalities.

--- server/utils/sendEmailToSubscribers.js
+++ server/utils/sendEmailToSubscribers.js
@@ -1,16 +1,26 @@
 import Subscriber from "../models/Subscriber.js";
 import { transporter } from "../config/mailer.js";
+import { generateEmailTemplate } from "./emailTemplate.js";
 
-export const sendEmailToSubscribers = async (title, content) => {
+export const sendEmailToSubscribers = async (title, content, slug) => {
   const subscribers = await Subscriber.find();
   const emails = subscribers.map((sub) => sub.email);
 
   if (emails.length === 0) return;
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    bcc: emails,
-    subject: title,
-    html: content,
-  });
+  for (const sub of subscribers) {
+    const html = generateEmailTemplate(
+      title,
+      content,
+      slug,
+      sub.unsubscribeToken
+    );
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: sub.email,
+      subject: title,
+      html,
+    });
+  }
 };
