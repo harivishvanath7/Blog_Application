4ab9cfb77d0f384d70c0181d2d8385bc639fc811
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-08 13:07:40 +0530

    Added Host And Port For Mail Service.

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,7 +1,12 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config(); // 
+
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
