39e8342067f869a8a6aaae43fe8e81888204c624
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-08 12:51:19 +0530

    feat: Updated Auto-Redirect functionality when Logged in.

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AdminLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      navigate("/admin/dashboard");
+    }
+  }, [navigate]);
 
   const handleLogin = async () => {
-    const res = await axios.post("http://localhost:5000/api/auth/login", {
-      email,
-      password,
-    });
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
 
-    localStorage.setItem("token", res.data.token);
-    alert("Logged in!");
+      localStorage.setItem("token", res.data.token);
+      alert("Logged in!");
+
+      navigate("/admin/dashboard");
+    } catch (error) {
+      alert("Login Failed!!");
+      console.error(error);
+    }
   };
 
   return (
