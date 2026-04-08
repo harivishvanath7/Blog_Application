499637705c2f1c2fc46019970e7fb5aa5ca9b251
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-08 12:40:10 +0530

    feat: Updated Routes for Imported Admin Pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,44 @@ import BlogDetail from "./pages/BlogDetail.jsx";
 import AdminLogin from "./pages/AdminLogin.jsx";
 import CreateBlog from "./pages/CreateBlog.jsx";
 
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import EditBlog from "./pages/EditBlog.jsx";
+import Navbar from "./components/Navbar.jsx"
+
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
+      <div className="min-h-screen pt-20">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:slug" element={<BlogDetail />} />
           <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin/create" element={<CreateBlog />} />
+          <Route
+            path="/admin/create"
+            element={
+              <ProtectedRoute>
+                <CreateBlog />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditBlog />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
